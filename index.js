67f58c6ed5c0e9bfe5c9c956ba8fb35ff4d98ba8
DESKTOP-Q6BLETQ\heojs <heo0842js@gmail.com>
2020-08-04 20:59:01 +0900

    convert text

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type' : 'text/plain'});
-    response.write('Hello Nodejs');
+    response.write('Hello Nodejs testtesttest');
     response.end();
 }).listen(process.env.PORT);
